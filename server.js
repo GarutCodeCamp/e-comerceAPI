a157e0ba5abc8c56f172046164875da38bb038ca
GarutCodeCamp <akuncode391@gmail.com>
2021-10-24 00:35:34 +0700

    create product

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { connect } = require('mongoose');
 const userUpdate = require('./routes/user');
 const auth = require('./routes/auth');
-
+const product = require('./routes/product');
 const app = express();
 
 connect(process.env.MONGODB).then(() => console.log('connect to db')).catch(err => console.log(err))
@@ -11,7 +11,7 @@ connect(process.env.MONGODB).then(() => console.log('connect to db')).catch(err 
 app.use(express.json());
 app.use('/auth', auth);
 app.use('/users', userUpdate);
-
+app.use('/product', product);
 
 
 
